dacb6bb17d5f429f6780aca1afafff446462f0c7
Orpaz Goldstein <orpgol@gmail.com>
2014-05-14 10:25:39 +0300

    Sync with function in need of debug
    First to debug wins a pizza.
    @orpgol @NataliGil @yuvadm

--- Demo_page/includes/js.js
+++ Demo_page/includes/js.js
@@ -1,18 +1,24 @@
-			function animate(targetElement, speed)
-			{
-    
-				$(targetElement).css({left:'-200px'});
-				$(targetElement).animate(
-					{
-					'left': $(document).width() + 200
-					}, 
-					{ 
-					duration: speed, 
-					complete: function(){
-						animateMe(this, speed);
-						}
-					}
-				);
-	
-			};
+$(function() 
+{
 
+
+    var $test = $('#test');
+    var initText = $.trim($test.text()), ptr = 0;
+    var timer = setInterval(function() {
+        var ln = $.trim($test.find('.trans').text().length);
+        if (ln == initText.length) {
+            $test.empty();
+            clearInterval(timer);
+        }
+
+        $('#test').html(function() {    
+
+            return $('<span>').addClass('removeMe')
+                .html(initText.substring(ptr++ , ptr))
+                .before($('<span>').addClass('trans').
+                       html(initText.substring(0 , ptr-1)))
+                .after(initText.substring(ptr));
+        }).find('span.removeMe').animate({'opacity': 0}, 200);
+                
+    }, 300);
+});
