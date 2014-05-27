02c82a42b8dc87691d7ab0401bc89c1b2480c981
Orpaz Goldstein <orpgol@gmail.com>
2014-05-27 13:03:23 +0300

    Collision detection + Listenter events
    The zombie now has collision detection, the dynamic object we found
    gets eaten by the zombie…
    plus, the zombie now has a listener, on click the zombie dies….

--- Extension/includes/zombie-walk.js
+++ Extension/includes/zombie-walk.js
@@ -1,7 +1,38 @@
-function start(location)
+function collision() 
+{
+		$(function() 
+		{
+			console.log("first");
+			var $test = $('.overlay');
+			var initText = $.trim($test.text()), ptr = 0;
+			var timer = setInterval(function() 
+			{
+				var ln = $.trim($test.find('.trans').text().length);
+				if (ln == initText.length) 
+				{
+					$test.empty();
+					clearInterval(timer);
+				}
+
+				$('.overlay').html(function() 
+				{    
+					console.log("second");
+					return $('<span>').addClass('removeMe')
+						.html(initText.substring(ptr++ , ptr))
+						.before($('<span>').addClass('trans').
+							   html(initText.substring(0 , ptr-1)))
+						.after(initText.substring(ptr));
+				}).find('span.removeMe').animate({'opacity': 0}, 200);}, 300);
+		});
+}
+
+
+function start(location, x_location)
 	{
 		// create an new instance of a pixi stage
-		var stage = new PIXI.Stage(0x66FF99);
+		// the second parameter is interactivity...
+		var interactive = true;
+		var stage = new PIXI.Stage(0x66FF99, interactive);
 		// create a renderer instance
 
 		var renderer = PIXI.autoDetectRenderer(1400, 1000, null, true);
@@ -33,6 +64,12 @@ function start(location)
 		var bunny = new PIXI.MovieClip(animationTextures);
 		bunny.stop();
 		bunny.visible = true;
+		bunny.setInteractive(true);
+		bunny.click = function click_func()
+		{
+			console.log("Clicked");
+			this.visible = false;
+		}
 
 		// center the sprites anchor point
 		bunny.anchor.x = 0.5;
@@ -50,6 +87,7 @@ function start(location)
 		var msPerFrame = 200;
 		var walkCycleFrameCount = 5;
 		var dirIndex = 1;
+		var detected = 0;
 
 		function animate() 
 		{
@@ -58,6 +96,16 @@ function start(location)
 			bunny.position.x -= 0.2;
 			//bunny.rotation -= 0.1;
 			//console.log(bunny.position.x +" "+ bunny.position.y); //position of bunny
+			
+			if (Math.round(bunny.position.x) == Math.round(x_location) && detected == 0)
+			{	// collision detection
+				console.log("yes");
+				detected = 1;
+				//collision();
+				var element = document.getElementsByTagName('span')
+				var obj = element[0].style.display='none';
+				
+			}
 	
 			bunny.gotoAndStop((Math.floor(animationAgeInMs / msPerFrame) % walkCycleFrameCount));
 			// render the stage   
@@ -98,7 +146,8 @@ $(document).ready(function find()
 				var element = document.getElementsByTagName('span')
 				var obj = element[0].getBoundingClientRect() //get html object location for collision 
 				console.log(obj.top); //position of objects top
+				console.log(obj.left); //position of objects top
 				
 				if (obj.top > 0)
-					start(obj.top);
+					start(obj.top, obj.left);
 			});
