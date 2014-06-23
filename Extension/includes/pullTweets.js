6cf48564cb0059e53cf40497c5329ae7e1e40498
Orpaz Goldstein <orpgol@gmail.com>
2014-06-23 15:02:47 +0300

    PHP pulls Twitter Json ready!

---
+++ Extension/includes/pullTweets.js
@@ -0,0 +1,24 @@
+$(document).ready(function() // pull from php function
+{
+	var index = 0;					// first index of loop
+	var sIndex = 0;					// second index of loop
+	var text = []					// message
+	var createdAt = [];				// message time
+	$.getJSON("http://shenkar.html5-book.co.il/2013-2014/html5/dev_17/tweets_json.php",function(data)
+	{
+		$.each(data,function(key,val)
+		{
+			while(index < data.length)
+			{	// this while is running all over the json file
+				$("#twitter_update_list").append("<p>" + data[index].created_at + "<br>" + data[index].text + "<br><br>" + "</p>");
+
+				console.log(data[index].text);
+				index++;
+			}
+					
+		});
+	});
+	
+	
+});
+
